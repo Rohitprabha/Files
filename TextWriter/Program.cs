52912ed5da5fc3f300643e3a012599f851db128f
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-28 21:06:21 +0530

    Add files via upload

---
+++ TextWriter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Files_TextWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter writer = File.CreateText("text.txt");
+            try
+            {
+                writer.Write("Hi");
+                writer.WriteLine("Good Evening \n How is your Day");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Data written successfully...");
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
