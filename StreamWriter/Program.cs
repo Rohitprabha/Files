6fdc0c54319401acd0cac5b707f8ed401d8cb993
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-28 20:51:55 +0530

    Add files via upload

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Files_StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("text.txt");
+            string temp = string.Empty;
+            do
+            {
+                Console.Write("Enter your text [-1 to exit]:");
+                temp = Console.ReadLine();
+                if (temp != "-1")
+                {
+                    sw.WriteLine(temp);
+                }
+            } while (temp != "-1");
+            sw.Close();
+
+            string line;
+            using (StreamReader sr = new StreamReader("text.txt"))     // The using statement also closes the StreamReader.
+            {
+                while ((line = sr.ReadLine()) != null)              // Read and show each line from the file.
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
