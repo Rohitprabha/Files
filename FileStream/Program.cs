7fc80f2701486ff13683d65b09f8406e6f4021a7
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-28 20:55:15 +0530

    Add files via upload

---
+++ FileStream/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Files_FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream f = new FileStream(@"C:\Users\Rohit Dasari\source\repos\Files_FileStream\bin\Debug\netcoreapp3.1\text.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            for (int i = 65; i <= 90; i++)
+            {
+                f.WriteByte((byte)i);
+            }
+            f.Close();
+        }
+    }
+}
