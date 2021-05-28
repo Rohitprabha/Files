0eca4a9e701b4fcf898daf14522dce06c5c336c7
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-28 20:44:29 +0530

    Add files via upload

---
+++ Files Example 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace File_Example_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Rohit Dasari\source\repos\Files Example 1\bin\Debug\netcoreapp3.1\MyDirectory";
+
+            string[] dirs = Directory.GetDirectories(path);             //to get all the Directories
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+
+            string[] files = Directory.GetFiles(path);                  //to get all the files
+            foreach (string f in files)
+            {
+                Console.WriteLine(f);
+            }
+
+            string source = @"C:\Users\Rohit Dasari\source\repos\Files Example 1\bin\Debug\netcoreapp3.1\MyDirectory\myTextFile.txt";
+            string dest = @"C:\Users\Rohit Dasari\source\repos\Files Example 1\bin\Debug\netcoreapp3.1\MyDirectory\text.txt";
+
+            File.Copy(source, dest, true);                                    //to copy it into the new file, true is to override the file
+            Console.WriteLine("Copied");
+
+            string dest1 = @"C:\Users\Rohit Dasari\source\repos\Files Example 1\bin\Debug\netcoreapp3.1\MyDirectory\SubDirectory";
+            string path1 = @"C:\Users\Rohit Dasari\source\repos\Files Example 1\bin\Debug\netcoreapp3.1\MyDirectory\text.txt";
+
+
+            File.Delete(path1);                                             //to delete a file in directory
+            Console.WriteLine("Deleted");
+
+        }
+    }
+}
